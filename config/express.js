635df0b18ebccea56f8b6493e174ae28c68c5fa2
Connor Hindley <conn.hindley@gmail.com>
2014-10-30 14:00:19 -0400

    add cookie parser

--- config/express.js
+++ config/express.js
@@ -4,6 +4,7 @@ var glob = require('glob');
 
 var logger = require('morgan');
 var compress = require('compression');
+var cookieParser = require('cookie-parser');
 
 module.exports = function(app, config) {
   app.set('views', config.root + '/app/views');
@@ -11,6 +12,7 @@ module.exports = function(app, config) {
 
   app.use(logger('dev'));
   app.use(compress());
+  app.use(cookieParser());
   app.use(express.static(config.root + '/public'));
   var oneDay = 86400000;
   app.use('/labs/labs', express.static(config.root + '/public', { maxAge: oneDay }));
