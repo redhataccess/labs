99600864e3f2a330c3541f6904c7110111bceb55
Ian Page Hands <ihands@redhat.com>
2014-10-30 16:54:00 -0400

    strip lang dupes

--- app/controllers/listing.js
+++ app/controllers/listing.js
@@ -1,34 +1,64 @@
+/*global require, module, _*/
 'use strict';
 
-var express = require('express'),
-  router = express.Router(),
-  mongoose = require('mongoose'),
-  Lab = mongoose.model('Lab');
+var express  = require('express'),
+    router   = express.Router(),
+    mongoose = require('mongoose'),
+    isDev    = false,
+    lodash   = require('lodash'),
+    Lab      = mongoose.model('Lab');
 
-var isDev = false;
+
 module.exports = function(app) {
-  if (app.get('env') === 'development') {
-    isDev = true;
-  }
-  app.use('/', router);
+    if (app.get('env') === 'development') {
+        isDev = true;
+    }
+    app.use('/', router);
 };
 
 router.get('/labs/', function(req, res, next) {
-  Lab.find(function(err, labs) {
-    if (err) return next(err);
-    if (req.get('Content-Type') === 'application/json') {
-      return res.json(labs);
-    }
-    var featured = labs.filter(function(lab) {
-      return lab.featured;
+    var my_lang = req.cookies.rh_locale,
+        langs = { en : 'en' };
+
+    langs[my_lang] = my_lang;
+    langs[req.cookies.rh_locale] = req.cookies.rh_locale;
+
+    Lab.find({lang: { $in: Object.keys(langs) } }).lean().exec(function(err, labs) {
+        if (err) {
+            return next(err);
+        }
+
+        if (my_lang !== 'en') {
+            lodash.remove(labs, function (obj, i, arr) {
+                if (obj.lang === 'en') {
+                    return lodash.find(arr, function (tmp_obj) {
+                        return tmp_obj.lang === my_lang && tmp_obj.lab_id === obj.lab_id;
+                    });
+                }
+                return false;
+            });
+        }
+
+        if (req.get('Content-Type') === 'application/json') {
+            return res.json(labs);
+        }
+
+        var featured = labs.filter(function(lab) {
+            return lab.featured === '1';
+        });
+
+        var isAdmin = (typeof req.query.admin !== 'undefined');
+
+        res.render('index', {
+            title: 'Labs - Red Hat Customer Portal',
+            featured: featured,
+            labs: labs,
+            isAdmin: isAdmin,
+            isDev: isDev
+        });
+
+        return null;
     });
-    var isAdmin = (typeof req.query.admin !== 'undefined');
-    res.render('index', {
-      title: 'Labs - Red Hat Customer Portal',
-      featured: featured,
-      labs: labs,
-      isAdmin: isAdmin,
-      isDev: isDev
-    });
-  });
+
+    return null;
 });
